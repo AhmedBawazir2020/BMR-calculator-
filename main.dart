6d9b6dfe38cb0d9196091d32b6139d0eda29c076
AhmedBawazir2020 <55338676+AhmedBawazir2020@users.noreply.github.com>
2019-10-08 20:41:28 +0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -11,80 +11,73 @@ class _MyAppState extends State<MyApp> {
   final TextEditingController _age = TextEditingController();
   final TextEditingController _weight = TextEditingController();
   final TextEditingController _height = TextEditingController();
-  double a, b,g,fmbi ,mbmi, result;
+  double a, b, g, fmbi, mbmi, result;
+
   @override
   Widget build(BuildContext context) {
     var color;
-        return MaterialApp(
-          title: 'Material App',
-          home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Color(color.hashCode),
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(color.hashCode),
           title: Text('BMR Calculator'),
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("\n "),
-                  Text("Gender"),
-                  new DropdownButton<String>(
-                    onChanged: popupButtonSelected,
-                    value: "Male",
-                    style: new TextStyle(
-                        fontSize: 25,
-                        color: const Color(0xFF202020),
-                        fontWeight: FontWeight.w200,
-                        fontFamily: "Merriweather"),
-                    items: <DropdownMenuItem<String>>[
-                      const DropdownMenuItem<String>(
-                          value: "Male", child: const Text("Male")),
-                      const DropdownMenuItem<String>(
-                          value: "Female", child: const Text("Female")),
-                    ],
-                  ),
-                  Text("\t\t\t "),
-                  Text("BMR Equation"),
-                  DropdownButton<String>(
-                    onChanged: popupButtonSelected,
-                    value: "Mifflin",
-                    
-                    style: new TextStyle(
-                        fontSize: 25,
-                        color: const Color(0xFF202020),
-                        fontWeight: FontWeight.w200,
-                        fontFamily: "Merriweather"),
-                    items: <DropdownMenuItem<String>>[
-                      const DropdownMenuItem<String>(
-                          value: "Mifflin", child: const Text("Mifflin ")),
-                      const DropdownMenuItem<String>(
-                          value: "Harris", child: const Text("Harris ")),
-                    ],
-                  ),
-                ],
-              ),
-              
               Text("Age"),
+              Divider(),
               TextField(
                 controller: _age,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(left: 200.0),
+                ),
               ),
+              Divider(),
               Text("Height"),
               TextField(
                 controller: _height,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(left: 200.0),
+                ),
               ),
+              Divider(),
               Text("Weight"),
+              Divider(),
               TextField(
                 controller: _weight,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(left: 200.0),
+                ),
               ),
-              RaisedButton(
-                child: Text("Calculater"),
-                shape: new RoundedRectangleBorder(borderRadius :BorderRadius.circular(20)),
-                onPressed: _onPress,
+              Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RaisedButton(
+                        child: Text("Calculater"),
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        onPressed: _onPress,
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text("Clear"),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: _clear,
+                  ),
+                ],
               ),
-              Text("BMI Male  : $mbmi"),
-              Text("BMI Female : $fmbi")
+              Text("BMI For Male     : $mbmi"),
+              Text("BMI For Female : $fmbi"),
             ],
           ),
         ),
@@ -97,11 +90,18 @@ class _MyAppState extends State<MyApp> {
       a = double.parse(_height.text);
       b = double.parse(_weight.text);
       g = double.parse(_age.text);
-      mbmi = (10*b)+(6.25*a)-(5*g)+5;
-      fmbi = (10*b)+(6.25*a)-(5*g)-161;
-      
+      mbmi = (10 * b) + (6.25 * a) - (5 * g) + 5;
+      fmbi = (10 * b) + (6.25 * a) - (5 * g) - 161;
     });
   }
 
   void popupButtonSelected(String value) {}
+
+  void _clear() {
+    setState(() {
+      this._height.text = '';
+      this._weight.text = '';
+      this._age.text = '';
+    });
+  }
 }
